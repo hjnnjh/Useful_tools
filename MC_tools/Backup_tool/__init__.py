ee0effaa4245b24d306312af0aa93d39d9eb26ce
hjnnjh <755423541@qq.com>
2020-02-15 15:10:20 +0800

    add mc_tools

---
+++ MC_tools/Backup_tool/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020-02-15 11:00
+# @Author  : Jinnan Huang
+# @Site    : 
+# @File    : __init__.py
+# @Software: PyCharm
+
---
+++ MC_tools/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020-02-14 17:24
+# @Author  : Jinnan Huang
+# @Site    : 
+# @File    : __init__.py
+# @Software: PyCharm
+
+"""some MC tools"""
